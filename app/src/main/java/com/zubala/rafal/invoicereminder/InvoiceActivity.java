bdd44e38c6e61703e1bcd26884e4869bf0a4b5c7
Rafał Zubala <rzubala@operatus.org>
2018-03-03 17:07:17 +0100

    Date picker

--- app/src/main/java/com/zubala/rafal/invoicereminder/InvoiceActivity.java
+++ app/src/main/java/com/zubala/rafal/invoicereminder/InvoiceActivity.java
@@ -1,17 +1,24 @@
 package com.zubala.rafal.invoicereminder;
 
-import android.content.ContentValues;
-import android.net.Uri;
+import android.app.DatePickerDialog;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class InvoiceActivity extends AppCompatActivity {
+
+    private EditText dateField;
+
+    private Calendar myCalendar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +29,35 @@ public class InvoiceActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        myCalendar = Calendar.getInstance();
+        dateField = (EditText) findViewById(R.id.dateField);
+        attachDatePicker();
+    }
+
+    private void attachDatePicker() {
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateDateField();
+            }
+
+        };
+        dateField.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(InvoiceActivity.this, date, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+
+    private void updateDateField() {
+        String myFormat = "dd/MM/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+        dateField.setText(sdf.format(myCalendar.getTime()));
     }
 
     @Override
