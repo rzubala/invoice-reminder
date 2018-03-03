906e9d55be5cbf7a63d2080452fbdcf6251d88b2
Rafał Zubala <rzubala@operatus.org>
2018-03-03 16:51:25 +0100

    item selector

--- app/src/main/java/com/zubala/rafal/invoicereminder/InvoiceActivity.java
+++ app/src/main/java/com/zubala/rafal/invoicereminder/InvoiceActivity.java
@@ -1,10 +1,15 @@
 package com.zubala.rafal.invoicereminder;
 
+import android.content.ContentValues;
+import android.net.Uri;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class InvoiceActivity extends AppCompatActivity {
 
@@ -26,5 +31,24 @@ public class InvoiceActivity extends AppCompatActivity {
             NavUtils.navigateUpFromSameTask(this);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onClickAddInvoice(View view) {
+        /*
+        String input = ((EditText) findViewById(R.id.editTextTaskDescription)).getText().toString();
+        if (input.length() == 0) {
+            return;
+        }
+
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(TaskContract.TaskEntry.COLUMN_DESCRIPTION, input);
+        contentValues.put(TaskContract.TaskEntry.COLUMN_PRIORITY, mPriority);
+
+        Uri uri = getContentResolver().insert(TaskContract.TaskEntry.CONTENT_URI, contentValues);
+        if(uri != null) {
+            Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_LONG).show();
+        }
+        */
+        finish();
     }
 }
